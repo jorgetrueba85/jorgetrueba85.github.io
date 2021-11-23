4c97971eeed4814d429a4aab5b7184bad9407a7c
Jorge Trueba <j.ed.tru.ga2@gmail.com>
2021-11-22 21:42:27 -0600

    .

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 function redirect(){
     
 }
+
+/*function scrolled(){*/
+    window.pageYOffset == window.scrollY;
+    var y = window.scrollY;
+    let root = document.documentElement;
+    
+    var h1opa = '1' - y / 300;
+    var h1opaneg = '0' + y / 500;
+    var why = y - 700;
+    console.log(y);
+
+    if (h1opa < 0){
+        document.getElementById('uname').innerHTML = 'Jorge Trueba';
+        document.getElementById('uname').style.opacity = h1opaneg;
+    }
+    else if (h1opa > 0){
+        document.getElementById('uname').innerHTML = 'jorgetrueba85';
+        document.getElementById('uname').style.opacity = h1opa;
+    }
+    
+    if(y > 700){
+        root.style.setProperty('--h1why', '-' + why / 2 + 'px');
+    }
+
+    if (y == 0){
+        why = 0;
+    }
+
+
